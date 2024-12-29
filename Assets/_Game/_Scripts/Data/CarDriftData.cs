cef08a1d99d23c564e091124887ef2e103275b28
Ekrem Atakan Kuloğlu <atakankuloglu1997@gmail.com>
2024-12-29 17:43:03 +0300

    Add car drift data

---
+++ Assets/_Game/_Scripts/Data/CarDriftData.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class CarDriftData
+{
+    [SerializeField] private float defaultSpeed;
+    [SerializeField] private float pressedSpeed;
+    [SerializeField] private float speedLerpFactor;
+    [SerializeField] private float swerveSpeed;
+    [SerializeField] private float swerveLimit;
+    [SerializeField] private float driftRotation;
+    [SerializeField] private float rotationSmoothness;
+    [SerializeField] private float wheelTurnAngle;
+    [SerializeField] private float wheelTurnSmoothness;
+
+    public float DefaultSpeed => defaultSpeed;
+    public float PressedSpeed => pressedSpeed;
+    public float SpeedLerpFactor => speedLerpFactor;
+    public float SwerveSpeed => swerveSpeed;
+    public float SwerveLimit => swerveLimit;
+    public float DriftRotation => driftRotation;
+    public float RotationSmoothness => rotationSmoothness;
+    public float WheelTurnAngle => wheelTurnAngle;
+    public float WheelTurnSmoothness => wheelTurnSmoothness;
+}
--- Assets/_Game/_Scripts/Entities/Player/PlayerCarEntity.cs
+++ Assets/_Game/_Scripts/Entities/Player/PlayerCarEntity.cs
@@ -7,18 +7,8 @@ public class PlayerCarEntity : MonoBehaviour
 
     [SerializeField] private Transform frontLeftWheel;
     [SerializeField] private Transform frontRightWheel;
-
-    [SerializeField] private float defaultSpeed = 10f;
-    [SerializeField] private float pressedSpeed = 20f;
-    [SerializeField] private float speedLerpFactor = 5f;
-
-    [SerializeField] private float swerveSpeed = 5f;
-    [SerializeField] private float swerveLimit = 5f;
-    [SerializeField] private float driftRotation = 15f;
-    [SerializeField] private float rotationSmoothness = 0.1f;
-
-    [SerializeField] private float wheelTurnAngle = 30f; // Maximum wheel turn angle
-    [SerializeField] private float wheelTurnSmoothness = 0.2f;
+    [SerializeField] private CarDriftData carDriftData;
+    
 
     private float _currentSpeed;
     private float _currentSwerve;
@@ -32,7 +22,7 @@ public class PlayerCarEntity : MonoBehaviour
     public void Initialize()
     {
         _transform = transform;
-        _currentSpeed = defaultSpeed;
+        _currentSpeed = carDriftData.DefaultSpeed;
         _lastPosition = _transform.position;
     }
 
@@ -61,16 +51,16 @@ public class PlayerCarEntity : MonoBehaviour
 
     private void DetermineTargetSpeedByInput()
     {
-        _targetSpeed = _inputDataProvider.IsPressing ? pressedSpeed : defaultSpeed;
-        _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, speedLerpFactor * Time.deltaTime);
+        _targetSpeed = _inputDataProvider.IsPressing ? carDriftData.PressedSpeed : carDriftData.DefaultSpeed;
+        _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, carDriftData.SpeedLerpFactor * Time.deltaTime);
     }
 
     private void HandleSwerve()
     {
-        float swerveDelta = _inputDataProvider.HorizontalInput * swerveSpeed * Time.deltaTime;
+        float swerveDelta = _inputDataProvider.HorizontalInput * carDriftData.SwerveSpeed * Time.deltaTime;
 
         _currentSwerve += swerveDelta;
-        _currentSwerve = Mathf.Clamp(_currentSwerve, -swerveLimit, swerveLimit);
+        _currentSwerve = Mathf.Clamp(_currentSwerve, -carDriftData.SwerveLimit, carDriftData.SwerveLimit);
 
         Vector3 targetPosition = new Vector3(_lastPosition.x + _currentSwerve, _transform.position.y, _transform.position.z);
         _transform.position = targetPosition;
@@ -78,15 +68,15 @@ public class PlayerCarEntity : MonoBehaviour
 
     private void HandleRotation()
     {
-        _targetRotation = -_inputDataProvider.HorizontalInput * driftRotation;
-        _currentRotation = Mathf.Lerp(_currentRotation, _targetRotation, rotationSmoothness);
+        _targetRotation = -_inputDataProvider.HorizontalInput * carDriftData.DriftRotation;
+        _currentRotation = Mathf.Lerp(_currentRotation, _targetRotation, carDriftData.RotationSmoothness);
         _transform.rotation = Quaternion.Euler(0, _currentRotation, 0);
     }
 
     private void RotateWheels()
     {
-        float targetWheelAngle = _inputDataProvider.HorizontalInput * wheelTurnAngle;
-        _currentWheelAngle = Mathf.Lerp(_currentWheelAngle, targetWheelAngle, wheelTurnSmoothness);
+        float targetWheelAngle = _inputDataProvider.HorizontalInput * carDriftData.WheelTurnAngle;
+        _currentWheelAngle = Mathf.Lerp(_currentWheelAngle, targetWheelAngle, carDriftData.WheelTurnSmoothness);
 
         // Apply rotation to wheels
         frontLeftWheel.localRotation = Quaternion.Euler(0, _currentWheelAngle, 0);
@@ -95,7 +85,7 @@ public class PlayerCarEntity : MonoBehaviour
 
     private void ResetWheelRotation()
     {
-        _currentWheelAngle = Mathf.Lerp(_currentWheelAngle, 0, wheelTurnSmoothness);
+        _currentWheelAngle = Mathf.Lerp(_currentWheelAngle, 0, carDriftData.WheelTurnSmoothness);
 
         // Reset wheel rotation to default
         frontLeftWheel.localRotation = Quaternion.Euler(0, _currentWheelAngle, 0);
@@ -105,7 +95,7 @@ public class PlayerCarEntity : MonoBehaviour
     private void ReturnToDefaultRotation()
     {
         _targetRotation = 0f;
-        _currentRotation = Mathf.Lerp(_currentRotation, _targetRotation, rotationSmoothness);
+        _currentRotation = Mathf.Lerp(_currentRotation, _targetRotation, carDriftData.RotationSmoothness);
         _transform.rotation = Quaternion.Euler(0, _currentRotation, 0);
 
         _lastPosition = _transform.position;
