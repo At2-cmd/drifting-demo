88ebdc94efc6b79be98a61c44ceb83957943bc94
Ekrem Atakan Kuloğlu <atakankuloglu1997@gmail.com>
2024-12-30 13:09:37 +0300

    Edit coin generation offset value

--- Assets/_Game/_Scripts/Entities/Road/GenerateableObjectBase.cs
+++ Assets/_Game/_Scripts/Entities/Road/GenerateableObjectBase.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public abstract class GenerateableObjectBase : MonoBehaviour
 {
     [SerializeField] protected int GenerationAmount;
+    [SerializeField] protected float OffsetBetweenGeneratedObjects;
     protected int CurrentlyGeneratedCount;
     protected Vector3 TempGenerationPosition;
-    protected float OffsetBetweenGeneratedObjects = 10;
     protected float InitialGenerationOffset = 25;
     protected List<IGenerateableObjectOnRoad> GeneratedObjectsList = new();
     protected float[] RoadXPositions = new float[3] { -2.25f, 0f, 2.25f };
